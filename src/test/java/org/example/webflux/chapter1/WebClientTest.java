dbc11e89a7b242d9ecee54d54efba720cded6962
sylee <lsyllll@naver.com>
2026-03-18 13:44:48 +0900

    WebFlux의 기본적인 사용법, Flux와 Mono

--- src/test/java/org/example/webflux/chapter1/WebClientTest.java
+++ src/test/java/org/example/webflux/chapter1/WebClientTest.java
@@ -1,10 +1,9 @@
-package org.example.webflux;
+package org.example.webflux.chapter1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 @SpringBootTest
 public class WebClientTest {
